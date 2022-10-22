f565907216b88ef9fcad0d37ca5b66dce49acb4e
SvetlanaPopenko <spopenko02@gmail.com>
2022-10-23 00:20:42 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const arr = [];
+const listRef = document.querySelector(`#ingredients`);
+
+for (let i = 0; i < ingredients.length; i += 1) {
+  const ingredient = document.createElement(`li`);
+  ingredient.classList.add(`item`);
+  ingredient.textContent = ingredients[i];
+
+  arr.push(ingredient);
+}
+
+listRef.append(...arr);
