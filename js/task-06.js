279e3a5a099267b1d8b2087b16d2ea17a553f7d7
SvetlanaPopenko <spopenko02@gmail.com>
2022-10-27 13:03:47 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,13 +1,13 @@
 const textInput = document.querySelector("#validation-input");
 
 function onBlur(evt) {
-    if (evt.currentTarget.value.length === Number(textInput.dataset.length)) {
-        textInput.classList.remove("invalid");
-        textInput.classList.add("valid");
-    } else {
-        textInput.classList.remove("valid");
-        textInput.classList.add("invalid");
-    } 
+  if (evt.currentTarget.value.length === Number(textInput.dataset.length)) {
+    textInput.classList.remove("invalid");
+    textInput.classList.add("valid");
+  } else {
+    textInput.classList.remove("valid");
+    textInput.classList.add("invalid");
+  }
 }
 
 textInput.addEventListener("blur", onBlur);
