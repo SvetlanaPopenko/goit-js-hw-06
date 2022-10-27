810cbd8779a5e5bdf4e2e36b67ac82372fa03cfc
SvetlanaPopenko <spopenko02@gmail.com>
2022-10-27 16:06:09 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -2,16 +2,15 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const buttonClick = document.querySelector('.change-color');
-const colorValue = document.querySelector('.color');
-const body = document.querySelector('body');
+const buttonClick = document.querySelector(".change-color");
+const colorValue = document.querySelector(".color");
+const body = document.querySelector("body");
 
-buttonClick.addEventListener('click', onchangeColor);
+buttonClick.addEventListener("click", onchangeColor);
 
 function onchangeColor() {
   const randomHexColor = getRandomHexColor();
 
   colorValue.textContent = `${randomHexColor}`;
   body.style.backgroundColor = `${randomHexColor}`;
-
 }
